a34b19761b7f9375de86adef7fcfd197f39b794a
BAJATEX <ventas.unicotextil@gmail.com>
2026-03-28 01:59:32 -0700

    Actualizar serverSupabase.js

--- src/lib/serverSupabase.js
+++ src/lib/serverSupabase.js
@@ -4,6 +4,7 @@ const url = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || 
 const secretKey =
   process.env.SUPABASE_SECRET_KEY ||
   process.env.SUPABASE_SERVICE_ROLE_KEY ||
+  process.env.SUPABASE_SERVICE_ROLE ||
   "";
 
 export function serverSupabase() {
